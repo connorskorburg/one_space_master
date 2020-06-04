9a2ffd1c1f41ccd56a8d19f80067b8631f2a83d2
Connor <connorskorburg@gmail.com>
2020-06-04 17:59:08 -0500

    added new dynamic images to home page

--- playlistofchoice/views.py
+++ playlistofchoice/views.py
@@ -9,8 +9,25 @@ import datetime
 
 # Create your views here.
 def index(request):
-
-    return render(request,'home.html')
+    cid = 'fcabf1c8ee3d4bbc81275366fd4bfacf'
+    secret = '39c9ebdb80bb41d89de106bb59e4e205'
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    results = sp.new_releases(country=None, limit=20, offset=0)
+    new_realeases_list = []
+    for i in results['albums']['items']:
+        new_realeases_list.append(
+            {
+                "artist": i['artists'][0]['name'],
+                "album_id": i['id'],
+                "album": i['name'],
+                "image_url": i['images'][0]['url'],
+            }
+        )
+    context = {
+        'new_releases_list': new_realeases_list
+    }
+    return render(request,'home.html', context)
 
 def sign_in(request):
     client_id = '93d03c51a99146ed992ca0175f68674b'
